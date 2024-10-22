6620735520b08259d85e00029aa6f548a0508935
Im Big Jet Plane <lamcz0407@gmail.com>
2024-10-23 01:24:56 +0700

    first commit

---
+++ turn_2fa.py
@@ -0,0 +1,18 @@
+import requests
+import http.cookiejar
+import urllib.parse
+
+import json 
+import re
+import hashlib
+# super useful time.sleep 
+import time
+from fake_useragent import UserAgent
+import Module_MailPhone
+import names
+from spotify import getcode_1, getcode_2
+import random
+
+
+
+
